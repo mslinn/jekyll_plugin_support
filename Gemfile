9043ca749c110fbf15e57573e3c9ca8ac9ba163b
Mike Slinn <mslinn@mslinn.com>
2023-02-20 11:14:28 -0500

    -

--- Gemfile
+++ Gemfile
@@ -3,13 +3,13 @@ source "https://rubygems.org"
 # Specify the gem's production dependencies in jekyll_plugin_support.gemspec
 gemspec
 
-gem "bundler", require: false
-
 gem 'debase', "0.2.5.beta2", require: false
-# gem 'debase-ruby_core_source', '0.10.18', require: false
 gem "rake", require: false
 gem 'rspec', require: false
 gem 'rubocop', require: false
+gem 'rubocop-md', require: false
+gem 'rubocop-performance', require: false
+gem 'rubocop-rake', require: false
 gem 'rubocop-rspec', require: false
 gem 'ruby-debug-ide', require: false
 
--- spec/jekyll_block_plugin_support_spec.rb
+++ spec/jekyll_block_plugin_support_spec.rb
@@ -1,41 +1,17 @@
 require 'jekyll_plugin_logger'
-# require 'rspec/match_ignoring_whitespace'
+require 'rspec/match_ignoring_whitespace'
 require_relative '../lib/jekyll_plugin_support'
 require_relative '../lib/jekyll_plugin_support_spec_support'
 
-# Lets get this party started
 class MyTest
   RSpec.describe JekyllSupport::JekyllBlock do
-    let(:logger) do
-      PluginMetaLogger.instance.new_logger(self, PluginMetaLogger.instance.config)
+    it 'has a bogus test' do
+      result = "<div class='quote'> This is the quoted text. </div>".dup
+      expect(result).to match_ignoring_whitespace <<~END_RESULT
+        <div class='quote'>
+          This is the quoted text.
+        </div>
+      END_RESULT
     end
-
-    let(:parse_context) { TestParseContext.new }
-
-    # Example for plugin authors to refer to:
-    #
-    # let(:helper) do
-    #   JekyllTagHelper.new(
-    #     'quote',
-    #     "cite='This is a citation' url='https://blah.com' This is the quoted text.",
-    #     logger
-    #   )
-    # end
-    #
-    # fit 'is created properly' do
-    #   command_line = "cite='This is a citation' url='https://blah.com' This is the quoted text.".dup
-    #   quote = Jekyll::Quote.send(
-    #     :new,
-    #     'quote',
-    #     command_line,
-    #     parse_context
-    #   )
-    #   result = quote.send(:render_impl, command_line)
-    #   expect(result).to match_ignoring_whitespace <<-END_RESULT
-    #     <div class='quote'>
-    #       This is the quoted text.
-    #     </div>
-    #   END_RESULT
-    # end
   end
 end
