261c5dfd84402a3eb69456885168f001e974b213
Mike Slinn <mslinn@mslinn.com>
2023-04-07 08:53:44 -0400

    -

--- lib/gem_support.rb
+++ lib/gem_support.rb
@@ -3,6 +3,8 @@ module GemSupport
   # @param file must be a fully qualified file name that points to a file within a gem.
   # @return Gem::Specification of gem that file points into, or nil if not called from a gem
   def self.current_spec(file)
+    return nil unless file.exist?
+
     searcher = if Gem::Specification.respond_to?(:find)
                  Gem::Specification
                elsif Gem.respond_to?(:searcher)
@@ -10,9 +12,7 @@ module GemSupport
                end
 
     searcher&.find do |spec|
-      # The File.fnmatch pattern ** means to match directories recursively or files expansively.
-      # This makes the method return the proper Gem::Specification when pointed at any file in any directory within a gem.
-      File.fnmatch(File.join(spec.full_gem_path, '**'), file)
+      file.start_with? spec.full_gem_path
     end
   end
 end
