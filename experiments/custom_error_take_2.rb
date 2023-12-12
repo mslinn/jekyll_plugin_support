cb7aba3a1ae2cd81724f4db53ee62f182d70a588
Mike Slinn <mslinn@mslinn.com>
2023-12-12 15:33:17 -0500

    -

--- experiments/custom_error_take_2.rb
+++ experiments/custom_error_take_2.rb
@@ -5,34 +5,32 @@ require 'facets/string/snakecase'
 
 # Prototype StandardError subclass.
 # It just needs to be created with the right class name
-module M1
-  class CustomError < StandardError
-    # These attributes are stored in the class so instances to store Jekyll context.
-    # The stored context allows subclasses to be created by any code,
-    # without having to worry about how to obtain the values in the context.
-    class << self
-      attr_accessor :argument_string, :line_number, :path, :tag_name
-    end
-
-    # Instance attributes; will contain the same values as the class attributes
+class CustomError < StandardError
+  # These attributes are stored in the class so instances can be created with these values.
+  # The stored context allows instances to be created from anywhere,
+  # without having to worry about how to obtain the values in the context.
+  class << self
     attr_accessor :argument_string, :line_number, :path, :tag_name
+  end
 
-    def self.factory(error_class_name)
-      return if Object.const_defined? error_class_name
+  # Instance attributes; will contain the same values as the class attributes
+  attr_accessor :argument_string, :line_number, :path, :tag_name
 
-      puts "Defining #{error_class_name}"
-      eval "#{error_class_name} = Class.new M1::CustomError" # rubocop:disable Style/EvalWithLocation, Security/Eval
-    end
+  def self.factory(error_class_name)
+    return if Object.const_defined? error_class_name
 
-    # Make copies of the class attributes as instance variables
-    def initialize(msg)
-      super(msg)
-      klass = self.class
-      @argument_string = klass.argument_string
-      @line_number = klass.line_number
-      @path = klass.path
-      @tag_name = klass.tag_name
-    end
+    puts "Defining #{error_class_name}"
+    eval "#{error_class_name} = Class.new CustomError" # rubocop:disable Style/EvalWithLocation, Security/Eval
+  end
+
+  # Make copies of the class attributes as instance variables
+  def initialize(msg)
+    super(msg)
+    klass = self.class
+    @argument_string = klass.argument_string
+    @line_number = klass.line_number
+    @path = klass.path
+    @tag_name = klass.tag_name
   end
 end
 
@@ -40,8 +38,9 @@ end
 module M2
   class C
     def initialize
-      @error_name = "#{self.class.name.camelcase(:upper)}Error" # => CError
-      M1::CustomError.factory @error_name
+      @error_name = "#{self.class.name.split('::').last.camelcase(:upper)}Error" # => CError
+      CustomError.factory "::#{@error_name}"
+      puts Object.const_defined?(:CError)
     end
 
     def set_error_context(argument_string, line_number, path, tag_name)
@@ -56,8 +55,8 @@ end
 
 c = M2::C.new                  # => Defining M2::CError
 puts "c is an #{c.class.name}" # => c is an M2::C
-puts "defined? M2::CError                => #{defined? M2::CError}"                # => "constant"
-puts "Object.const_defined? 'M2::CError' => #{Object.const_defined? 'M2::CError'}" # => true
+puts "defined? CError                => #{defined? CError}" # => "constant"
+puts "Object.const_defined? 'CError' => #{Object.const_defined? :CError}" # => true
 
 # The point of the example is to be able to establish the error context before creating any instances.
 # The following would be called from JekyllTag.initialize:
@@ -67,8 +66,8 @@ c.set_error_context('blah', 123, '/', 'my_tag')
 # Creating the error copies the class-level context to the instance.
 # This allows the new error class instance to behave just like any other StandardError subclass.
 begin
-  raise M2::CError, 'asdf'
-rescue M2::CError => e
+  raise CError, 'asdf'
+rescue CError => e
   puts "The class variables of #{e.class.name} are:"
   e.class.instance_variables.each do |name|
     puts "  #{name}=#{e.class.instance_variable_get name}"
@@ -78,6 +77,6 @@ rescue M2::CError => e
     puts "  #{name}=#{e.instance_variable_get name}"
   end
   puts <<~END_MSG
-    Caught an #{e.class.name} with message #{e.message}
+    Caught a #{e.class.name} with message #{e.message}
   END_MSG
 end
